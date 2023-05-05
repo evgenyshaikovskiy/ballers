75dc7a05c93f1d4768e57908be6fcd81f4104c3a
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2023-05-06 00:15:58 +0300

    add static serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: 'postgres://ballers_postgresql_user:9NMteVx8GZFtKZt5zeLbtyW6U4AncqkN@dpg-chamehorddl1r9vovcl0-a.frankfurt-postgres.render.com/ballers_postgresql?ssl=true',
+        port: 5432,
+        username: 'ballers_postgresql_user',
+        database: 'ballers_postgresql',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api/(.*)'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
